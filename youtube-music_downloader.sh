f8006ff2e8853094cab9db50c74c74a6fd7e030a
elias <14819853+eliasfrehner@users.noreply.github.com>
2021-02-21 21:51:56 +0100

    Add files via upload

--- youtube-music_downloader.sh
+++ youtube-music_downloader.sh
@@ -26,6 +26,7 @@ echo
 echo "press $(tput setaf 1)ENTER$(tput sgr0) to start the download"
 read
 ~/Downloads/youtube-dl -ciw -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o ~/Downloads/Music/"%(artist)s - %(title)s.%(ext)s" "$url"
+clear
 echo "FINISH"
 sleep 1
 echo "press $(tput setaf 1)ENTER$(tput sgr0) to open the Folder with the music. Stored in Downloads -> Music"
