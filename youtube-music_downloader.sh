6185bfb1fd302524403505102f2f79e2ef97390c
elias <14819853+eliasfrehner@users.noreply.github.com>
2021-02-21 21:43:42 +0100

    Add files via upload

--- youtube-music_downloader.sh
+++ youtube-music_downloader.sh
@@ -6,27 +6,24 @@ echo "$(tput setaf 1)youtube-music_downloads$(tput sgr0) $(tput setaf 2)by$(tput
 sleep 2s
 echo
 echo
+clear
 echo "Welcome $(tput setaf 5)$(whoami)$(tput sgr0)"
 sleep 1
 echo
-echo "the tool will help you to download music from youtube music"
-sleep 3s
-echo
-echo
+echo "this tool will help you to download music from YouTube Music"
+sleep 2s
 echo
 echo "press $(tput setaf 1)ENTER$(tput sgr0) to continue"
 read
 mkdir ~/Downloads/Music
 clear
-echo "paste in the playlist-url or music url"
+echo "paste in the playlist-url or title-url"
 read url
 clear
+echo "$(tput setaf 1)Your URL:$(tput sgr0) $url"
 echo
 echo
-echo "ur url: $url"
-echo
-echo
-echo "should i start to download?"
+echo "press $(tput setaf 1)ENTER$(tput sgr0) to start the download"
 read
 ~/Downloads/youtube-dl -ciw -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o ~/Downloads/Music/"%(artist)s - %(title)s.%(ext)s" "$url"
 echo "FINISH"
